5fdb16812704ff2c2967f826f47f3608b71905c1
Jefferson Alves <jefferson.alves.costa@gmail.com>
2016-09-23 05:00:37 -0300

    no message

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 rmdir /s /q "release"
-pyinstaller.exe --distpath "release" --workpath "tmp" --specpath "tmp" --clean -F -y -n Tera -i "icon.ico" -c --uac-admin main.py
+pyinstaller.exe --distpath "./" --workpath "tmp" --specpath "tmp" --clean -F -y -n Tera -i "icon.ico" -c --uac-admin main.py
 rmdir /s /q "tmp"
 xcopy "data" "release\data" /i /q /e /y
