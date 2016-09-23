f321d84a6c3ef5340649a20d772be4c38ba9dd4d
Jefferson Alves <jefferson.alves.costa@gmail.com>
2016-09-23 04:52:21 -0300

    no message

--- main.py
+++ main.py
@@ -1,35 +1,10 @@
-import os
-import sys
-
 import argparse
 
 from core.block_splitter import BlockSplitter
 from core.connection_handler import ConnectionHandler
 from core.messages_handler import MessagesHandler
 from net.sniffer import Sniffer
-import win32com.shell.ShellExecuteEx
 
-
-def elevate():
-    import ctypes, win32event, win32process
-    outpath = r'%s\%s.out' % (os.environ["TEMP"], os.path.basename(__file__))
-    if ctypes.windll.shell32.IsUserAnAdmin():
-        if os.path.isfile(outpath):
-            sys.stderr = sys.stdout = open(outpath, 'w', 0)
-        return
-    with open(outpath, 'w+', 0) as outfile:
-        hProc = win32com.shell.ShellExecuteEx(lpFile=sys.executable, \
-            lpVerb='runas', lpParameters=' '.join(sys.argv), fMask=64, nShow=0)['hProcess']
-        while True:
-            hr = win32event.WaitForSingleObject(hProc, 40)
-            while True:
-                line = outfile.readline()
-                if not line: break
-                sys.stdout.write(line)
-            if hr != 0x102: break
-    os.remove(outpath)
-    sys.stderr = ''
-    sys.exit(win32process.GetExitCodeProcess(hProc))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tera sniffer')
